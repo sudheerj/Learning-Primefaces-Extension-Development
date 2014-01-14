869282ca52ed0d527637c60f4fa8249812704cb1
sudheerj <sudheer.jonna@gmail.com>
2014-01-14 14:07:25 +0530

    JobHub ckEditor component updated

--- jobhub/src/main/java/com/packt/pfextensions/controller/AdminController.java
+++ jobhub/src/main/java/com/packt/pfextensions/controller/AdminController.java
@@ -25,6 +25,12 @@ public class AdminController implements Serializable {
 	private String privacy;
 	private String disclaimer;
 	private String help;
+	private String color1="1234FF";
+	private String color2="1234FF";
+	private String color3="1234FF";
+	private String color4="1234FF";
+	private String color5="1234FF";
+	
 	private String language;
 
 	private Boolean aboutusFlag;
@@ -39,15 +45,14 @@ public class AdminController implements Serializable {
 
 	@PostConstruct
 	public void init() {
-		System.out.println("application scoped bean");
 		images = new ArrayList<String>();
 
 		for (int i = 1; i <= 6; i++) {
 			images.add("galleria" + i + ".jpg");
 		}
-		aboutus = "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts."
-				+ "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts."
-				+ "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts.";
+		aboutus = "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts.";
+				/*+ "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts."
+				+ "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts.";*/
 		communication = "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts.";
 		privacy = "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts.";
 		disclaimer = "The jobhub application is aimed to provide employement for most of the people around the globe.This is mainly categoriezed for jobseekers and employeers.Administrator can have access rights to change the rules and acts.";
@@ -61,6 +66,7 @@ public class AdminController implements Serializable {
 	}
 
 	public void aboutusSave() {
+		aboutus = aboutus.replaceAll("\\<.*?>","");
 		aboutusFlag = false;
 	}
 
@@ -69,6 +75,7 @@ public class AdminController implements Serializable {
 	}
 
 	public void communicationSave() {
+		communication = communication.replaceAll("\\<.*?>","");
 		communicationFlag = false;
 	}
 
@@ -77,6 +84,7 @@ public class AdminController implements Serializable {
 	}
 
 	public void privacySave() {
+		privacy = privacy.replaceAll("\\<.*?>","");
 		privacyFlag = false;
 	}
 
@@ -85,6 +93,7 @@ public class AdminController implements Serializable {
 	}
 
 	public void disclaimerSave() {
+		disclaimer = disclaimer.replaceAll("\\<.*?>","");
 		disclaimerFlag = false;
 	}
 
@@ -93,6 +102,7 @@ public class AdminController implements Serializable {
 	}
 
 	public void helpSave() {
+		help = help.replaceAll("\\<.*?>","");
 		helpFlag = false;
 	}
 
@@ -191,5 +201,58 @@ public class AdminController implements Serializable {
 	public void setLanguage(String language) {
 		this.language = language;
 	}
+
+
+	public String getColor1() {
+		return color1;
+	}
+
+
+	public void setColor1(String color1) {
+		this.color1 = color1;
+	}
+
+
+	public String getColor2() {
+		return color2;
+	}
+
+
+	public void setColor2(String color2) {
+		this.color2 = color2;
+	}
+
+
+	public String getColor3() {
+		return color3;
+	}
+
+
+	public void setColor3(String color3) {
+		this.color3 = color3;
+	}
+
+
+	public String getColor4() {
+		return color4;
+	}
+
+
+	public void setColor4(String color4) {
+		this.color4 = color4;
+	}
+
+
+	public String getColor5() {
+		return color5;
+	}
+
+
+	public void setColor5(String color5) {
+		this.color5 = color5;
+	}
+
+
+	
 
 }
